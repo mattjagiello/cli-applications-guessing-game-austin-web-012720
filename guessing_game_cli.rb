d6f73825854ccf8965fc21f0bfcf40e63d833547
Matthew Jagiello <mjagiello@gmail.com>
2020-01-28 05:16:33 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,8 @@
 def run_guessing_game
-  rand(1..6)
+  random_num = rand(1..6)
   input = gets "Guess a number between 1 and 6"
   if input == "exit"
     puts "Goodbye!"
+  elsif input =
   end
 end
